24ee38287dbcba694a89d317b50fc053759fcf79
Uzair Khan <muhammaduzair751@gmail.com>
2020-07-01 04:09:13 -0700

    adding mongodb deriver to api

--- api/database/config.ts
+++ api/database/config.ts
@@ -1,7 +1,28 @@
 import { MongoClient } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
 
-const client = new MongoClient();
 
-client.connectWithUri("mongodb://<denoland>:<denoland687>@ds259738.mlab.com:59738/denologin");
+class DB {
+    public client: MongoClient;
+    constructor(public dbName: string, public url: string) {
+        this.dbName = dbName;
+        this.url = url;
+        this.client = {} as MongoClient;
+    }
+    connect() {
+        const client = new MongoClient();
+        client.connectWithUri(this.url);
+        this.client = client;
+    }
+    getDatabase() {
+        return this.client.database(this.dbName);
+    }
 
-// mongodb://<denoland>:<denoland687>@ds259738.mlab.com:59738/denologin
+}
+
+const dbName = "apidb";
+const dbHostUrl = "mongodb://localhost:27017";
+const db = new DB(dbName, dbHostUrl);
+db.connect();
+
+export default db;
+
--- api/routes/routes.ts
+++ api/routes/routes.ts
@@ -2,16 +2,52 @@ import { RouterContext } from "https://deno.land/x/oak@v5.0.0/mod.ts";
 import { renderFileToString } from "https://deno.land/x/dejs@0.7.0/mod.ts";
 import { hashSync, compareSync } from "https://deno.land/x/bcrypt@v0.2.1/mod.ts";
 import { makeJwt, setExpiration, Jose } from "https://deno.land/x/djwt@v0.9.0/create.ts";
+import db from './../database/config.ts';
 
 
+//define user schema
+interface userSchema {
+    _id: { $oid: string },
+    username: string,
+    email: string,
+    password: string
 
-export const login = async (ctx: RouterContext) => {
-    const body = await ctx.request.body();
-    console.log(body.value);
-    ctx.response.body= body.value;
 }
 
+const database = db.getDatabase();
+const users = database.collection('users');
+
 export const register = async (ctx: RouterContext) => {
+    try {
+        const body = await ctx.request.body();
+        const { email, username, password } = body.value;
+        console.log(body);
+        if (!Object.keys(body.value).length) {
+            ctx.response.status = 400;
+            ctx.response.body = { "error": "Please provide details to login" };
+        } else {
+          await  users.findOne(
+                $or :[
+                    {
+                        email: email
+                    },
+                    {
+                        username : username
+                    }
+                ]
+            ).then(user =>{
+                
+            });
+        }
+
+
+    } catch (error) {
+        ctx.response.status = 500;
+        ctx.response.body = { err: error.toString() };
+    }
+}
+
+export const login = async (ctx: RouterContext) => {
     const body = await ctx.request.body();
     console.log("i am register");
     ctx.response.body = "in register";
